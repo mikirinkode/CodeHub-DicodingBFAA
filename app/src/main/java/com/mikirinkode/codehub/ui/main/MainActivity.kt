e3315996c92ceaf3a26836363e70553091be41d2
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-05-02 20:38:29 +0700

    add side nav / drawer navigation

--- app/src/main/java/com/mikirinkode/codehub/ui/main/MainActivity.kt
+++ app/src/main/java/com/mikirinkode/codehub/ui/main/MainActivity.kt
@@ -1,142 +1,57 @@
 package com.mikirinkode.codehub.ui.main
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.CompoundButton
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.mikirinkode.codehub.data.model.UsersResponse
+import androidx.core.view.GravityCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.mikirinkode.codehub.R
 import com.mikirinkode.codehub.databinding.ActivityMainBinding
-import com.mikirinkode.codehub.ui.detailuser.DetailUserActivity
-import com.mikirinkode.codehub.ui.favorite.FavoriteActivity
-import com.mikirinkode.codehub.ui.main.settings.MainViewModel
-import com.mikirinkode.codehub.ui.main.settings.SettingPreferences
-import com.mikirinkode.codehub.ui.main.settings.ViewModelFactory
 import com.mikirinkode.codehub.ui.search.SearchActivity
 
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(){
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
-    private lateinit var usersViewModel: UsersViewModel
-    private lateinit var usersAdapter: UsersAdapter
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        showLoading(true)
 
+        setSupportActionBar(binding.appBarMain.toolbar)
 
-        usersAdapter = UsersAdapter()
-        usersViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[UsersViewModel::class.java]
+        supportActionBar?.elevation = 0f
 
-
-        usersViewModel.isLoading.observe(this, {
-            showLoading(it)
-        })
-        usersViewModel.onFailure.observe(this, {
-            onFailure(it)
-        })
-
-
-        usersViewModel.getListUsers().observe(this, {
-            if (it != null) {
-                onFailure(false)
-                usersAdapter.setList(it)
-            }
-        })
-
-
-        usersAdapter.setOnItemClickCallback(object: UsersAdapter.OnItemClickCallback {
-            override fun onItemClicked(data: UsersResponse) {
-                Intent(this@MainActivity, DetailUserActivity::class.java).also {
-                    it.putExtra(DetailUserActivity.EXTRA_USERNAME, data.username)
-                    it.putExtra(DetailUserActivity.EXTRA_ID, data.id)
-                    it.putExtra(DetailUserActivity.EXTRA_AVATAR_URL, data.avatarUrl)
-                    it.putExtra(DetailUserActivity.EXTRA_HTML_URL, data.htmlUrl)
-                    startActivity(it)
-                }
-            }
-
-        })
-
-        binding.apply {
-            rvUsers.layoutManager = LinearLayoutManager(this@MainActivity)
-            rvUsers.setHasFixedSize(true)
-            rvUsers.adapter = usersAdapter
-
-            val pref = SettingPreferences.getInstance(dataStore)
-            val mainViewModel = ViewModelProvider(this@MainActivity, ViewModelFactory(pref))[MainViewModel::class.java]
-            mainViewModel.getThemeSettings().observe(this@MainActivity, {
-                    isDarkModeActive: Boolean ->
-                if (isDarkModeActive){
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                    switchTheme.isChecked = true
-                }
-                else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    switchTheme.isChecked = false
-                }
-            })
-            switchTheme.setOnCheckedChangeListener { _: CompoundButton, isChecked:Boolean ->
-                mainViewModel.saveThemeSetting(isChecked)
-
-            }
-
-            btnFav.setOnClickListener{
-                val moveToFavActivity = Intent(this@MainActivity, FavoriteActivity::class.java)
-                startActivity(moveToFavActivity)
-            }
-
-            floatBtnSearch.setOnClickListener {
-                val moveToSearchActivity = Intent(this@MainActivity, SearchActivity::class.java)
-                startActivity(moveToSearchActivity)
-            }
-        }
-
-        refreshApp()
+        val drawerLayout = binding.drawerLayout
+        val navView = binding.sideNavView
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_favorite,
+                R.id.nav_settings
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
-
-    private fun refreshApp() {
-        binding.apply {
-            swipeToRefresh.setOnRefreshListener {
-                usersViewModel.findUsers()
-                swipeToRefresh.isRefreshing = false
-            }
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return super.onSupportNavigateUp() || navController.navigateUp(appBarConfiguration)
     }
 
-
-    private fun showLoading(state: Boolean) {
-        binding.apply {
-            if (state) {
-                progressBar.visibility = View.VISIBLE
-            } else {
-                progressBar.visibility = View.GONE
-            }
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen((GravityCompat.START))){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
-
-    private fun onFailure(fail: Boolean) {
-        binding.apply {
-            if (fail) {
-                tvOnFailMsg.visibility = View.VISIBLE
-            } else {
-                tvOnFailMsg.visibility = View.GONE
-            }
-        }
-    }
-
-
 }
