c033e659c47b7bd9767b68cefea5ef42f09413d1
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-05-03 21:01:52 +0700

    adding hyperlink on click

--- app/src/main/java/com/mikirinkode/codehub/ui/otherapps/OtherAppsFragment.kt
+++ app/src/main/java/com/mikirinkode/codehub/ui/otherapps/OtherAppsFragment.kt
@@ -1,11 +1,13 @@
 package com.mikirinkode.codehub.ui.otherapps
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.mikirinkode.codehub.R
 import com.mikirinkode.codehub.databinding.FragmentOtherAppsBinding
@@ -27,18 +29,23 @@ class OtherAppsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            setImage(ivKotakMovie, R.drawable.kotak_movie)
-            setImage(ivSpod, R.drawable.spod)
-            setImage(ivTextScanner, R.drawable.text_scanner)
-            setImage(ivFinanciar, R.drawable.financiar)
+            setImage(ivKotakMovie, R.drawable.kotak_movie, "https://play.google.com/store/apps/details?id=com.mikirinkode.kotakmovie")
+            setImage(ivSpod, R.drawable.spod, "https://play.google.com/store/apps/details?id=com.mikirinkode.spod")
+            setImage(ivTextScanner, R.drawable.text_scanner, "https://github.com/mikirinkode/TextRecognition-Android")
+            setImage(ivFinanciar, R.drawable.financiar, "https://github.com/mikirinkode/")
         }
     }
 
-    private fun setImage(image: ImageView, drawable: Int) {
+    private fun setImage(image: ImageView, drawable: Int, projectUrl: String) {
         Glide.with(requireContext())
             .load(drawable)
             .fitCenter()
             .into(image)
+        image.setOnClickListener {
+            val uri: Uri = Uri.parse(projectUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 
 
